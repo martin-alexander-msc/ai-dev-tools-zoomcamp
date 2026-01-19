2f77d95128815b7d199c441454537fe5a850754b
Martin Alexander <martin.alexander.msc@gmail.com>
2026-01-19 13:02:01 +0000

    Add logic

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='todo_list_index'),
+    path('create/', views.create, name='todo_list_create'),
+    path('<int:todo_id>/edit/', views.edit, name='todo_list_edit'),
+    path('<int:todo_id>/toggle/', views.toggle_resolved, name='todo_list_toggle'),
+    path('<int:todo_id>/delete/', views.delete, name='todo_list_delete'),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,47 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
+
+from .forms import TodoForm
+from .models import Todo
+
 
 def index(request):
-    return HttpResponse("Todo List: OK")
+    todos = Todo.objects.order_by("is_resolved", "due_date", "-created_at")
+    form = TodoForm()
+    return render(request, "todo_list/index.html", {"todos": todos, "form": form})
+
+
+def create(request):
+    if request.method != "POST":
+        return redirect("todo_list_index")
+    form = TodoForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return redirect("todo_list_index")
+
+
+def edit(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect("todo_list_index")
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, "todo_list/edit.html", {"form": form, "todo": todo})
+
+
+@require_POST
+def toggle_resolved(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    todo.is_resolved = not todo.is_resolved
+    todo.save(update_fields=["is_resolved", "updated_at"])
+    return redirect("todo_list_index")
+
+
+@require_POST
+def delete(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    todo.delete()
+    return redirect("todo_list_index")
